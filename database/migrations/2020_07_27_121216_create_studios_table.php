cb8197d80de26068b813af6b34a1163e4ab5704e
root <trndo>
2020-07-27 12:20:28 +0000

    Add new migrations and models

---
+++ database/migrations/2020_07_27_121216_create_studios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudiosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('studios', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('subtitle');
+            $table->string('image');
+            $table->text('body');
+            $table->string('description');
+            $table->string('seo_title');
+            $table->string('seo_description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('studios');
+    }
+}
